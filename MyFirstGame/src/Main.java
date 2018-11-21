64ae6d393a6399a5f49619dcb0f770fd2447a3d0
tamer350 <duttanis@gmail.com>
2018-11-21 16:42:28 +1300

    Jpanel addition

--- MyFirstGame/src/Main.java
+++ MyFirstGame/src/Main.java
@@ -1,9 +1,34 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class Main {
-
+	
+	private int frameXSize = 1200;
+	private int frameYSize = 800;
+	private int frameXLoc = 350;
+	private int frameYLoc = 200;
+	
+	JFrame frame;
+	
+	public Main() {
+		setUpPanel();
+	}
+	
+	/*Just the panel set up*/
+	public void setUpPanel() {
+		JPanel panel = new JPanel();
+		frame = new JFrame();
+		frame.add(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setSize(this.frameXSize, this.frameYSize);
+		frame.setLocation(this.frameXLoc, this.frameYLoc);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		new Main();
 	}
 
 }
